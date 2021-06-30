7da6bc7cf19b4921e4022f47de9c1ef28fbce955
Vera Fan <shuyao.fan@gmail.com>
2021-06-30 09:01:31 -0400

    create table

---
+++ database.sql
@@ -0,0 +1,19 @@
+CREATE TABLE ccf2020(
+	user_ int,
+	card int,
+	year_ int,
+	month_ int,
+	day_ int,
+	time_ time,
+	amount varchar,
+	use_Chip varchar,
+	merchant_Name bigint,
+	merchant_City varchar,
+	merchant_State varchar,
+	zip float,
+	mcc int,
+	errors varchar,
+	is_fraud varchar
+	);
+	
+	SELECT * FROM ccf2020
